74f35b33699204d10ee847e65727d1b5b083689e
asleshapokhrel <aslesha.pokhrel@mail.utoronto.ca>
2018-12-04 15:20:11 -0500

    Added method to update is_read field

--- src/main/java/com/ftms/ftmsapi/repository/NotificationRepository.java
+++ src/main/java/com/ftms/ftmsapi/repository/NotificationRepository.java
@@ -2,9 +2,19 @@ package com.ftms.ftmsapi.repository;
 
 import com.ftms.ftmsapi.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    //SQL query to select rows that match notification_id
+    String Notification = "SELECT * FROM notifications WHERE notification_id = ?1";
+
+    @Query(value = Notification, nativeQuery = true)
+    Notification getNotification(Long notification_id);
 
 }
